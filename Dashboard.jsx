173331d6423783ca45f2aab62a72c5cb67581d09
Darmabalakanahesh A M <119047499+balaanthu@users.noreply.github.com>
2023-07-12 15:26:10 +0530

    Add files via upload

---
+++ Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from '../components/Sidebar'
+const Dashboard = () => {
+    return (
+        <div>
+            <Sidebar/>
+            <h1>dashboard page</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
